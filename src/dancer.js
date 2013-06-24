efcfed6c09938906411a232a4743fdd318f7e67b
Hack Reactor Students <github@hackreactor.com>
2013-06-24 12:33:10 -0700

    WIP on MakeBlinkyDancer constructor

--- src/dancer.js
+++ src/dancer.js
@@ -9,7 +9,7 @@ var MakeDancer = function(top, left, timeBetweenSteps){
 MakeDancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+  setTimeout(this.step, this.timeBetweenSteps);
 };
 
 MakeDancer.prototype.setPosition = function(top, left){
